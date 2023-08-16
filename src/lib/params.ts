b4fd7982b6fc44f6340ca86339ae81c73b60b040
Therry <94805938+TherryHilaire@users.noreply.github.com>
2023-08-15 21:12:56 -0400

    Update params.ts

--- src/lib/params.ts
+++ src/lib/params.ts
@@ -12,7 +12,7 @@ import {
 } from './types';
 import { Icons } from './utils';
 
-export const TITLE_SUFFIX = 'Slick template with Svelte';
+export const TITLE_SUFFIX = 'Therry Léonhard Hilaire | Freelancer';
 
 export const NavBar = {
 	home: 'Home',
@@ -43,35 +43,23 @@ export const getPlatfromIcon = (platform: Platform): Icons => {
 
 export const HOME: HomePageParams = {
 	title: 'Home',
-	name: 'Name',
-	lastName: 'LASTNAME',
+	name: 'Therry Léonhard',
+	lastName: 'Hilaire',
 	description:
-		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!',
+		"I have spent for the better part of 3 years learning software development, playing and making mistakes. And only last year, I've started developing apps. While being a developer is hard and decision fatigue is very common, I've fallen in love with the process. And it's now an important part of my life.",
 	links: [
-		{ platform: Platform.GitHub, link: 'https://github.com/' },
-		{
-			platform: Platform.Linkedin,
-			link: 'https://www.linkedin.com/'
-		},
+		{ platform: Platform.GitHub, link: 'https://github.com/TherryHilaire' },
 		{
 			platform: Platform.Twitter,
-			link: 'https://twitter.com/'
+			link: 'https://twitter.com/leo_hilaire'
 		},
 		{
 			platform: Platform.StackOverflow,
-			link: 'https://stackoverflow.com/'
+			link: 'https://stackoverflow.com/users/18946784/therry-l%c3%a9onhard-hilaire'
 		},
 		{
 			platform: Platform.Email,
-			link: 'riadh-adrani@hotmail.fr'
-		},
-		{
-			platform: Platform.Youtube,
-			link: 'https://www.youtube.com'
-		},
-		{
-			platform: Platform.Facebook,
-			link: 'https://www.facebook.com'
+			link: 'therryleseul@gmail.com'
 		}
 	]
 };
