93718e73dc22202281dbe590f19ae32e6ac33964
Therry <94805938+TherryHilaire@users.noreply.github.com>
2023-08-15 21:13:53 -0400

    Update projects.params.ts

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -4,32 +4,18 @@ import type { Project } from './types';
 
 const MY_PROJECTS: Array<Project> = [
 	{
-		slug: 'slick-portfolio-angular',
+		slug: 'old-portfolio-svelte',
 		color: '#5e95e3',
 		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore non dolores voluptatibus vitae praesentium aperiam, iure laboriosam repellendus sunt explicabo pariatur totam enim, nihil animi quisquam. Sit vero quod laborum!',
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
+			'My old portfolio built with Svelte in my early stage of freelancing. I set it up to have job oppurtunities can\'t say it worked.',
+		links: [{ to: 'https://therry-page.web.app/', label: 'Website' }, { to: 'https://github.com/TherryHilaire/portofolio-old', label: 'Github'}],
 		logo: Assets.Unknown,
-		name: 'Slick Portfolio',
+		name: 'Old Portfolio',
 		period: {
 			from: new Date()
 		},
-		skills: getSkills('angular', 'ts', 'tailwind'),
-		type: 'Website Template'
-	},
-	{
-		slug: 'slick-portfolio-svelte',
-		color: '#ff3e00',
-		description:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
-		logo: Assets.Svelte,
-		name: 'Slick Portfolio',
-		period: {
-			from: new Date()
-		},
-		skills: getSkills('svelte', 'ts', 'tailwind', 'sass'),
-		type: 'Website Template'
+		skills: getSkills('svelte', 'js', 'html'),
+		type: 'My old portfolio'
 	}
 ];
 
